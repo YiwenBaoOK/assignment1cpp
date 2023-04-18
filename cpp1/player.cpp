75d1cb144170e073f1873cfd4dec3a4943f22981
Yiwen Bao <baoyy005@mymail.unisa.edu.au>
2023-04-18 20:19:24 +1000

    working on player function

--- cpp1/player.cpp
+++ cpp1/player.cpp
@@ -1,1 +1,16 @@
 #include "player.h"
+#include <string>
+
+std::string player::names[] = { "Sam", "Billy", "Jen", "Bob", "Sally", "Joe", "Sue",
+"Sasha", "Tina", "Marge" };
+
+std::string player::_names()
+{
+	std::string value = "";
+	value = std::string(names[rand() % 10]);
+	return  value;
+}
+void player::addCardToTableau(card* card, CardCollection* hand) {
+	;
+
+}
