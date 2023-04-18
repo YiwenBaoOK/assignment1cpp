c1afb7926a46dd5c0908dc50401ad302697ce408
Yiwen Bao <baoyy005@mymail.unisa.edu.au>
2023-04-18 20:15:15 +1000

    adding game header files

---
+++ cpp1/game.h
@@ -0,0 +1,15 @@
+#include <vector>
+#include "card.h"
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <random>
+#include <algorithm>
+using namespace std;
+
+class game {
+
+public: static void startgame();
+
+};
+
