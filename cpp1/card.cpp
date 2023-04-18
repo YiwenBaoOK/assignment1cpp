ab9335ae3f2f17df405111b22ebd9aefe308e8f7
Yiwen Bao <baoyy005@mymail.unisa.edu.au>
2023-04-18 20:17:36 +1000

    working on card cpp files

--- cpp1/card.cpp
+++ cpp1/card.cpp
@@ -1,1 +1,33 @@
 #include "card.h"
+
+std::string str(card::CardType cardType) {
+    switch (cardType) {
+    case card::Tempura:
+        return "Tempura";
+        break;
+    case card::Sashimi:
+        return "Sashimi";
+        break;
+    case card::Nigiri_egg:
+        return "Nigiri(Egg)";
+        break;
+    case card::Nigiri_Salmon:
+        return "Nigiri(Salmon)";
+        break;
+    case card::Nigiri_Squid:
+        return "Nigiri(Squid)";
+        break;
+    case card::MakiRoll_1:
+        return "MakiRoll(1)";
+        break;
+    case card::MakiRoll_2:
+        return "MakiRoll(2)";
+        break;
+    case card::MakiRoll_3:
+        return "MakiRoll(3)";
+        break;
+    case card::Dumpling:
+        return "Dumpling";
+        break;
+    }
+};
